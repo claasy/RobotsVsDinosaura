f52b198455040e50ec7676897cdea256afefdd2d
Amanda <aclaas522@gmail.com>
2022-03-03 11:22:03 -0600

    dinosaur class built

---
+++ Dinosaur.py
@@ -0,0 +1,13 @@
+class Dinosaur:
+    def __init__ (self):
+        self.name = ""
+        self.attack_power = 100
+        self.health = 100
+
+    def set_name(self):
+        self.name=""
+    def attack(self):
+        self.attack_power
+    def health(self):
+        self.health = 100
+        
--- Robot.py
+++ Robot.py
@@ -3,10 +3,18 @@ from Weapon import Weapon
 
 class Robot:
     def __init__ (self):
-        self.name: ""
+        self.name = ""
         self.health: int
         self.weapon: Weapon
     
     def set_name(self):
-        self.name = Billy
+        self.name = ""
         print ("Robot name:", self.name)
+    def Weapon(self):
+        self.weapon = 100
+    def health(self):
+        self.health: 100
+        
+
+    
+   
