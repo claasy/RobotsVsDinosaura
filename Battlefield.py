a88bfd37c694027d11628f0407af30b6f0cdc786
Amanda <aclaas522@gmail.com>
2022-03-04 16:01:18 -0600

    final code

--- Battlefield.py
+++ Battlefield.py
@@ -1,22 +1,20 @@
 from Fleet import Fleet
 from Herd import Herd
 
-
 class Battlefield:
     def __init__ (self):
         self.fleet = Fleet()
         self.herd = Herd()
         pass
-
-    def fight(self): 
-        self.herd.dinosaurs[0].attack(self.fleet.robots[0])
-        self.fleet.robots[0].attack(self.herd.dinosaurs[0])
-        
-        # after an attack, if one getting attacked has 0 health, remove them from their list.
-        # continue looping as long as both lists have at least one thing in it
-
-        #who goes first? I want dinosaurs to go first
-        for health in :
-            if health == 0:
-                print("The game is over, the dinosaurs have won!")
-                pass
+    def fight(self):        
+        while(self.fleet.robots != [] and self.herd.dinosaurs != []):
+            self.herd.dinosaurs[0].attack(self.fleet.robots[0])
+            self.fleet.robots[0].attack(self.herd.dinosaurs[0])
+            if (self.fleet.robots[0].health <= 0):
+                self.fleet.robots.remove(self.fleet.robots[0])
+            if (self.herd.dinosaurs[0].health <= 0):
+                self.herd.dinosaurs.remove(self.herd.dinosaurs[0])    
+        if (self.fleet.robots == []):
+            print ("Dinosaurs won - Game over!")
+        elif (self.herd.dinosaurs == []):
+            print ("Robots won - Game over!")
